a0742bfc6ce2691729513eaf6dc79e4ee9c902e4
alanbrazoes <79315595+alanbrazoes@users.noreply.github.com>
2021-08-05 12:55:08 -0300

    laços

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,28 @@
+//EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO
+//1. O que o código abaixo está fazendo? Qual o resultado impresso no console?
+//Resposta: enquanto a variavel i for menr que 5 incrementa +1 e soma ao valor da variavel valor. E ao final do loop imprime 10.
+
+//2.a) O que vai ser impresso no console?
+//Resposta:19, 20, 21, 23, 25, 27, 30
+//b) Se eu quisesse acessar o índice de cada elemento dessa lista, o for...of... é suficiente? Se sim, o que poderia ser usado para fazer isso?
+//Resposta: 
+/*3. 
+  *
+  **
+  ***
+  **** */
+
+//EXERCÍCIO DE ESCRITA DE CÓDIGO
+//1.
+/* let bichinhos = Number(prompt('Quantos bichinhos de estimação você tem?'))
+let nomeBichinhos = []
+
+if(bichinhos > 0){
+    while((nomeBichinhos.length + 1) <= bichinhos){
+    let perguntaNomes = prompt('Qual o nome deles?')
+    nomeBichinhos.push(perguntaNomes)
+}
+}else{
+    console.log('Que pena! você pode adotar um pet.')
+}
+console.log(nomeBichinhos) */
