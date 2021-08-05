96db9b5c0e9920256537e1e49e4851162593be12
alanbrazoes <79315595+alanbrazoes@users.noreply.github.com>
2021-08-05 10:07:18 -0300

    quinzena2-projeto

---
+++ quinzena-02/blakjack/nossoJogo.js
@@ -0,0 +1,51 @@
+//1 - Imprime uma mensagem no console "Bem vindo ao jogo de Blackjack!".
+
+console.log('Bem vindo ao jogo Blackjack')
+let perguntaInicioDoJogo = confirm('Quer iniciar uma rodada?')
+
+if(perguntaInicioDoJogo === true){
+   function sorteioUsuario() {
+      //sorteio da primeira carta do usuário.
+      let primeiroSorteioUsuario = comprarCarta()
+      let primeiraCartaUsuario = [(primeiroSorteioUsuario.texto),(primeiroSorteioUsuario.valor)]
+      
+      //sorteio da segunda carta do usuário.
+      let segundoSorteioUsuario = comprarCarta()
+      let segundaCartaUsuario = [(segundoSorteioUsuario.texto),(segundoSorteioUsuario.valor)]
+      
+      //soma dos valores das cartas.
+      let somaValorUsuario = Number(primeiraCartaUsuario[1] + Number(segundaCartaUsuario[1]))
+      
+      //impressão do resultado do sorteio, junto a soma da pontuação das cartas.
+      console.log(`Usuário - Cartas: ${primeiraCartaUsuario[0]} ${segundaCartaUsuario[0]} - Pontuação ${somaValorUsuario}`)
+      
+      //peço o retorno do resultado para comparar quem venceu.
+      return somaValorUsuario
+   }
+   let chamaSorteioUsuario = sorteioUsuario()
+   let pontuacaoUsuario = chamaSorteioUsuario
+  
+   function sorteioComputador() {
+      //sorteio da primeira carta computador.
+      let primeiroSorteioComputador = comprarCarta()
+      let primeiraCartaComputador = [(primeiroSorteioComputador.texto), (primeiroSorteioComputador.valor)]
+
+      //sorteio segunda carta computador.
+      let segundoSorteioComputador = comprarCarta()
+      let segundaCartaComputador = [(segundoSorteioComputador.texto),(segundoSorteioComputador.valor)]
+
+      //soma dos valores das cartas sorteadas.
+      let somaValorComputador = Number(primeiraCartaComputador[1]) + Number(segundaCartaComputador[1])
+
+      //impressão do resultado dos sorteios, junto a soma da pontuação obtida pelo computador.
+      console.log(`Computador - Cartas: ${primeiraCartaComputador[0]} ${segundaCartaComputador[0]} - Pontuação ${somaValorComputador}`)
+
+      //peço o retorno do resultado para comparar quem venceu.
+      return somaValorComputador
+   }
+   let chamaSorteioComputador = sorteioComputador()
+   let pontuacaoComputador = chamaSorteioComputador
+
+}else{
+   console.log("Fim de jogo!")
+}
