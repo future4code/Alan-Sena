c6c0427ce491ed05097eea575b256a5fe5e118d9
alanbrazoes <79315595+alanbrazoes@users.noreply.github.com>
2021-08-06 13:27:33 -0300

    salvo1

--- quinzena2/projeto-blackjack/blakjack/desafio.js
+++ quinzena2/projeto-blackjack/blakjack/desafio.js
@@ -1,13 +1,74 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+//1 - Imprime uma mensagem no console "Bem vindo ao jogo de Blackjack!".
 
+console.log('Bem vindo ao jogo Blackjack')
+let perguntaInicioDoJogo = confirm('Quer iniciar uma rodada?')
+
+if(perguntaInicioDoJogo === true){
+   //SORTEIO CARTA USUARIO
+   function sorteioUsuario() {
+      //sorteio da primeira carta do usuário.
+      let primeiroSorteioUsuario = comprarCarta()
+      let primeiraCartaUsuario = [(primeiroSorteioUsuario.texto),(primeiroSorteioUsuario.valor)]
+      
+      //sorteio da segunda carta do usuário.
+      let segundoSorteioUsuario = comprarCarta()
+      let segundaCartaUsuario = [(segundoSorteioUsuario.texto),(segundoSorteioUsuario.valor)]
+      
+      //Verifica se tem duas cartas 'A'
+      let verificaCartaA = ((primeiraCartaUsuario[0].includes('A')) && (segundaCartaUsuario[0].includes('A')))
+      if(verificaCartaA === true){
+         sorteioUsuario()
+      }
+      
+      //soma dos valores das cartas.
+      let somaValorUsuario = Number(primeiraCartaUsuario[1] + Number(segundaCartaUsuario[1]))
+      
+      //impressão do resultado do sorteio, junto a soma da pontuação das cartas.
+      console.log(`Usuário - Cartas: ${primeiraCartaUsuario[0]} ${segundaCartaUsuario[0]} - Pontuação ${somaValorUsuario}`)
+      
+      //peço o retorno do resultado para comparar quem venceu.
+      return somaValorUsuario
+   }
+   let chamaSorteioUsuario = sorteioUsuario()
+   let pontuacaoUsuario = chamaSorteioUsuario
+   
+  //SORTEIO CARTA COMPUTADOR
+   function sorteioComputador() {
+      //sorteio da primeira carta computador.
+      let primeiroSorteioComputador = comprarCarta()
+      let primeiraCartaComputador = [(primeiroSorteioComputador.texto), (primeiroSorteioComputador.valor)]
+
+      //sorteio segunda carta computador.
+      let segundoSorteioComputador = comprarCarta()
+      let segundaCartaComputador = [(segundoSorteioComputador.texto),(segundoSorteioComputador.valor)]
+
+      //Verifica se tem duas cartas 'A'
+      let verificaCartaComputador = ((primeiraCartaComputador[0].includes('A')) && (segundaCartaComputador[0].includes('A')))
+      if(verificaCartaComputador === true){
+         sorteioComputador()
+      }
+
+      //soma dos valores das cartas sorteadas.
+      let somaValorComputador = Number(primeiraCartaComputador[1]) + Number(segundaCartaComputador[1])
+
+      //impressão do resultado dos sorteios, junto a soma da pontuação obtida pelo computador.
+      console.log(`Computador - Cartas: ${primeiraCartaComputador[0]} ${segundaCartaComputador[0]} - Pontuação ${somaValorComputador}`)
+
+      //peço o retorno do resultado para comparar quem venceu.
+      return somaValorComputador
+   }
+   let chamaSorteioComputador = sorteioComputador()
+   let pontuacaoComputador = chamaSorteioComputador
+   
+   //Quem venceu
+   if(pontuacaoUsuario > pontuacaoComputador){
+      console.log('O usuário venceu!')
+   }else if(pontuacaoComputador > pontuacaoUsuario){
+      console.log("O computador venceu!")
+   }else{
+      console.log("Empate!")
+   }
+ 
+}else{
+   console.log("Fim de jogo!")
+}
