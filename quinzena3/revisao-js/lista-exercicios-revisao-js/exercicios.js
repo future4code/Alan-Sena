fac3db00c08c22229c7fdbaf23710ff56b1f1d3c
alanbrazoes <79315595+alanbrazoes@users.noreply.github.com>
2021-08-15 12:12:22 -0300

    final

--- quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -278,15 +278,54 @@ function retornaPessoasNaoAutorizadas(pessoas) {
 
 // EXERCÍCIO 19A
 function ordenaPorNome(consultasNome) {
-  
+  for (let i = 0;i < consultasNome.length;i++) {
+    for (let j = 0;j < consultasNome.length - i - 1;j++) {
+      if (consultasNome[j].nome > consultasNome[j + 1].nome) {
+        const temp = consultasNome[j]
+        consultasNome[j] = consultasNome[j + 1]
+        consultasNome[j + 1] = temp
+      }
+    }
+  }
+  return consultasNome
 }
 
 // EXERCÍCIO 19B
 function ordenaPorData(consultasData) {
+  for (let i = 0;i < consultasData.length;i++) {
+    for (let j = 0;j < consultasData.length - i - 1;j++) {
 
+      const arrayData1 = consultasData[j].dataDaConsulta.split('/')
+      const dia1 = Number(arrayData1[0])
+      const mes1 = Number(arrayData1[1])
+      const ano1 = Number(arrayData1[2])
+
+      const arrayData2 = consultasData[j + 1].dataDaConsulta.split('/')
+      const dia2 = Number(arrayData2[0])
+      const mes2 = Number(arrayData2[1])
+      const ano2 = Number(arrayData2[2])
+
+      const data1 = new Date(ano1, mes1 - 1, dia1).getTime()
+      const data2 = new Date(ano2, mes2 - 1, dia2).getTime()
+
+      if (data1 > data2) {
+        const temp = consultasData[j]
+        consultasData[j] = consultasData[j + 1]
+        consultasData[j + 1] = temp
+      }
+    }
+  }
+  return consultasData
 }
 
 // EXERCÍCIO 20
 function calculaSaldo(contas) {
-
+  contas.forEach((conta) => {
+    let totalDeCompras = 0
+    conta.compras.forEach((valor) => {
+      totalDeCompras += valor
+    })
+    conta.saldoTotal -= totalDeCompras
+  })
+  return contas
 }
