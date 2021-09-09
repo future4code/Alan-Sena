a769e32055537353cae277bbf39554a23ee10d88
alanbrazoes <79315595+alanbrazoes@users.noreply.github.com>
2021-09-09 11:28:35 -0300

    pasta img

--- quinzena5/labedin/labedin/src/App.js
+++ quinzena5/labedin/labedin/src/App.js
@@ -10,8 +10,8 @@ function App() {
         <h2>Dados pessoais</h2>
         <CardGrande 
           imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          nome="Alan Marcos Brazões Sena" 
+          descricao="Oi, eu sou o Alan. Sou alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
         />
         
         <ImagemButton 
