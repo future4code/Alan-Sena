f5e2157a88f60ae05ef9c488c45671f9622bf4f0
alanbrazoes <79315595+alanbrazoes@users.noreply.github.com>
2021-09-16 21:20:00 -0300

    insta4

--- insta4/insta4/src/App.js
+++ insta4/insta4/src/App.js
@@ -7,29 +7,16 @@ const MainContainer = styled.div`
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  width: 100%;
+  height: 100%;
 `
 
 class App extends React.Component {
   render() {
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        
-        <Post
-          nomeUsuario={'alan'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
 
-        <Post
-          nomeUsuario={'lisa'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+        <Post />
 
       </MainContainer>
     );
--- insta4/insta4/src/components/Post/Post.js
+++ insta4/insta4/src/components/Post/Post.js
@@ -12,6 +12,7 @@ const PostContainer = styled.div`
   border: 1px solid gray;
   width: 300px;
   margin-bottom: 10px;
+  margin: 16px 0;
 `
 
 const PostHeader = styled.div`
@@ -39,14 +40,56 @@ const UserPhoto = styled.img`
 const PostPhoto = styled.img`
   width: 100%;
 `
+const SecaoInputs = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const Inputs = styled.input`
+  border-radius: 20px;
+  padding: 2px 8px;
+  border: 1px solid grey;
+  margin: 4px 0;
+`
+
+const Botao = styled.button`
+  border-radius: 20px;
+  border: 1px solid grey;
+  background-color: #fff;
+  cursor: pointer;
+`
 
 class Post extends React.Component {
+
   /* ======= ESTADO ========== */
+
   state = {
+    post: [],
+    usuario:"",
+    foto:"",
+    fotoUsuario:"",
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
     numeroComentarios: 0
+  }
+
+  postar = () => {
+    const novoPost = {
+      usuario: this.state.usuario,
+      fotoUsuario: this.state.fotoUsuario,
+      foto: this.state.foto
+    }
+
+    const novosPosts = [...this.state.post, novoPost]
+
+    this.setState({ post: novosPosts })
+
+    this.setState({usuario: ""})
+    this.setState({foto: ""})
+    this.setState({fotoUsuario: ""})
   }
 
   onClickCurtida = () => {
@@ -78,8 +121,58 @@ class Post extends React.Component {
     })
   }
 
+  onChangeUsuario = (e) => {
+    this.setState({usuario: e.target.value})
+  }
+  
+  onChangeFotoUsuario = (e) => {
+    this.setState({fotoUsuario: e.target.value})
+  }
+  
+  onChangeFoto = (e) => {
+    this.setState({foto: e.target.value})
+  }
+  
   render() {
+    let componenteComentario
     let iconeCurtida
+
+    const post = this.state.post.map((postagem) => {
+      return (
+        <PostContainer>
+          <PostHeader>
+
+            <UserPhoto 
+              src={this.state.fotoUsuario} 
+              alt={'Imagem do usuario'}
+            />
+
+            <p>{this.state.usuario}</p>
+
+          </PostHeader>
+
+          <PostPhoto src={this.state.foto} alt={'Imagem do post'}/>
+
+          <PostFooter>
+
+            <IconeComContador
+              icone={iconeCurtida}
+              onClickIcone={this.onClickCurtida}
+              valorContador={this.state.numeroCurtidas}
+            />
+
+            <IconeComContador
+              icone={iconeComentario}
+              onClickIcone={this.onClickComentario}
+              valorContador={this.state.numeroComentarios}
+            />
+            {componenteComentario}
+          </PostFooter>
+        </PostContainer>
+
+      )
+    })
+
 
     if(this.state.curtido === true) {
       iconeCurtida= iconeCoracaoPreto
@@ -87,35 +180,24 @@ class Post extends React.Component {
       iconeCurtida = iconeCoracaoBranco
     }
 
-    let componenteComentario
 
     if(this.state.comentando) {
       componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
-
-    return <PostContainer>
-      <PostHeader>
-        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
-        <p>{this.props.nomeUsuario}</p>
-      </PostHeader>
-
-      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
-
-      <PostFooter>
-        <IconeComContador
-          icone={iconeCurtida}
-          onClickIcone={this.onClickCurtida}
-          valorContador={this.state.numeroCurtidas}
-        />
-
-        <IconeComContador
-          icone={iconeComentario}
-          onClickIcone={this.onClickComentario}
-          valorContador={this.state.numeroComentarios}
-        />
-      </PostFooter>
-      {componenteComentario}
-    </PostContainer>
+    
+    return (
+      <div>
+        <SecaoInputs>
+          <Inputs value={this.state.usuario} onChange={this.onChangeUsuario} placeholder={"Usuario"}></Inputs>
+          <Inputs value={this.state.foto} onChange={this.onChangeFoto} placeholder={"foto"}></Inputs>
+          <Inputs value={this.state.fotoUsuario} onChange={this.onChangeFotoUsuario} placeholder={"foto usuario"}></Inputs>
+          <Botao onClick={this.postar}>POSTAR</Botao>
+        </SecaoInputs>
+        <div>
+          {post}
+        </div>
+      </div>
+    )
   }
 }
 
