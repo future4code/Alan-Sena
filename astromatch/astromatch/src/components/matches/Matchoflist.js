4c6e037e06deb424ca21dee699fd633ee167b0df
Alan Brazões <alanbrazoes@gmail.com>
2021-10-31 20:05:18 -0300

    final

--- astromatch/astromatch/src/components/matches/Matchoflist.js
+++ astromatch/astromatch/src/components/matches/Matchoflist.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { AiOutlineClear } from 'react-icons/ai'
+import { AiOutlineClear } from 'react-icons/ai';
 import styled from 'styled-components';
 
 const Lista = styled.div`
