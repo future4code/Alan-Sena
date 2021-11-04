4f5282bc7e5ce8bef7109a97afec1b243801e650
Alan Brazões <alanbrazoes@gmail.com>
2021-11-04 10:11:21 -0300

    inicial

---
+++ labex/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <div>
+     olá mundo
+    </div>
+  );
+}
+
+export default App;
