1ade781751044d874dad33a9226f714c578ed39c
Alan Brazões <alanbrazoes@gmail.com>
2021-12-01 21:30:55 -0300

    biblioteca MUI

--- labexfinal/src/App.js
+++ labexfinal/src/App.js
@@ -1,10 +1,12 @@
 import Router from "./router";
+import { theme } from "./theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
--- labexfinal/src/pages/publicArea/HomePage/index.js
+++ labexfinal/src/pages/publicArea/HomePage/index.js
@@ -1,7 +1,15 @@
+import Button from '@material-ui/core/Button';
+
 const HomePage = () => {
   return (
     <>
-    
+      <Button variant="contained" color="secondary">
+        Login
+      </Button>
+
+      <Button variant="contained" color={'secondary'}>
+        Trips
+      </Button>
     </>
   )
 }
